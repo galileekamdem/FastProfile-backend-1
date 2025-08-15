0406afd2883e8391b84856fc976793cbb3bc1890
Galilee.K <kkamdem071@gmail.com>
2025-08-15 07:31:32 +0100

    Initial commit: setup Django project with Django Ninja

---
+++ fastprofile/api.py
@@ -0,0 +1,10 @@
+from ninja import NinjaAPI, Schema
+
+api = NinjaAPI()
+
+class HelloSchema(Schema):
+    name: str = "world"
+
+@api.post("/hello")
+def hello(request, data: HelloSchema):
+    return f"Hello {data.name}"

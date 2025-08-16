34d0332c310f7ccd910dc3d3e5900c4e2e8a96e1
Galilee.K <kkamdem071@gmail.com>
2025-08-16 10:44:25 +0100

    Add /me endpoint with authentication check and schema

--- fastprofile/api.py
+++ fastprofile/api.py
@@ -2,9 +2,22 @@ from ninja import NinjaAPI, Schema
 
 api = NinjaAPI()
 
-class HelloSchema(Schema):
-    name: str = "world"
+class UserSchema(Schema):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
 
-@api.post("/hello")
-def hello(request, data: HelloSchema):
-    return f"Hello {data.name}"
+class Error(Schema):
+    message: str
+
+@api.get("/me", response={200: UserSchema, 403: Error})
+def me(request):
+    if not request.user.is_authenticated:
+        return 403, {"message": "Please sign in first"}
+    return 200, {
+        "username": request.user.username,
+        "email": request.user.email,
+        "first_name": request.user.first_name,
+        "last_name": request.user.last_name,
+    }
